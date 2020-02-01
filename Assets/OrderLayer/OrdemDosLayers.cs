773b674b52528fb21c26cecb3b049073c4174d3d
agehm <guto.gehm@gmail.com>
2020-02-01 19:05:34 -0300

    Iluminacao e orderlayers

---
+++ Assets/OrderLayer/OrdemDosLayers.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrdemDosLayers : MonoBehaviour
+{
+    [HideInInspector]
+    public SpriteRenderer MySpriteRenderer;
+    private int MyLayer;
+    private Transform MyPosition;
+    private OrderLayerManager LayerManagerScript;
+    private OrdemDosLayers MyScript;
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        MySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        MyPosition = gameObject.GetComponent<Transform>();
+        MyScript = gameObject.GetComponent<OrdemDosLayers>();
+        LayerManagerScript = GameObject.FindGameObjectWithTag("LayerManager").GetComponent<OrderLayerManager>();
+        LayerManagerScript.CalculatorOfLayer(MyScript, MyPosition.position.y);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            LayerManagerScript.CalculatorOfLayer(MyScript, MyPosition.position.y);
+        }
+    }
+
+    public void ChangeThisLayer(float y)
+    {
+
+    }
+    public void ChangeMyLayer(int change)
+    {
+        MySpriteRenderer.sortingOrder = change;
+    }
+
+}
---
+++ Assets/OrderLayer/OrderLayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderLayerManager : MonoBehaviour
+{
+
+
+    public void CalculatorOfLayer(OrdemDosLayers ScriptAtual, float y)
+    {
+        int OrderAtual;
+        y *= 100;
+        float ValorDoLayer = 1000 - y;
+
+        OrderAtual = (int)ValorDoLayer;
+
+        ScriptAtual.ChangeMyLayer(OrderAtual);
+    }
+
+}
