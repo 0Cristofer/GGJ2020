f901d4c8631b5c763f7415dccbaac1a06f641cdf
Kaish <kaish.kmail@gmail.com>
2020-02-02 14:18:46 -0200

    Update art assets

--- Assets/Scripts/BearGenerator/BearPiecesGenerator.cs
+++ Assets/Scripts/BearGenerator/BearPiecesGenerator.cs
@@ -24,7 +24,7 @@ public class BearPiecesGenerator : MonoBehaviour
         DefinePlayerPieces();
         CreateBearInGame();
         DefinePositionPieces();
-        //DefinePositionPiecesRest();
+        DefinePositionPiecesRest();
 
         print("//--------------------------------------//");
     }
